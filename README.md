78df94949157081de08d001461ff95e75c6ba18a
Hugo Giraudel <hugo.giraudel@gmail.com>
2013-11-25 22:37:50 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,69 @@
 Countdown.js
 ============
 
-A little JavaScript cutstomizable countdown
+Here is a little (vanilla) JavaScript customizable countdown.
+
+## Examples
+
+```javascript
+new Countdown();
+
+new Countdown({
+    selector: '#timer',
+    msgBefore: "Will start at Christmas!",
+    msgAfter: "Happy new year folks!",
+    msgPattern: "{days} days, {hours} hours and {minutes} minutes before new year!",
+    dateStart: new Date('Dec 25, 2013 12:00:00'),
+    dateEnd: new Date('Jan 1, 2014 12:00:00')
+});
+```
+
+## Options
+
+You can pass the constructor number of options, including:
+
+### `selector`
+
+The selector you want to inject Countdown into
+
+*Default*: `.timer`
+
+### `msgBefore`
+
+The message to display before reaching `dateStart`
+
+*Default*: `"Be ready!"`
+
+### `msgAfter`
+
+The message to display once reaching `dateEnd`
+
+*Default*: `"It's over, sorry folks!"`
+
+### `msgPattern`
+
+The message to display during the countdown where values between braces get replaced by actual numeric values.  
+Possible patterns:
+
+* `{years}`
+* `{months}`
+* `{weeks}`
+* `{days}`
+* `{hours}`
+* `{minutes}`
+* `{seconds}`
+
+*Default*: `"{days} days, {hours} hours, {minutes} minutes and {seconds} seconds left."`
+
+### `dateStart`
+
+The date to start the countdown to. Should be an instance of class `Date`. Documentation [here at MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date).
+
+*Default*: `new Date()`
+
+### `dateEnd`
+
+The date to end the countdown to. Should be an instance of class `Date`. Documentation [here at MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date).
+
+*Default*: `new Date(new Date().getTime() + (24 * 60 * 60 * 1000))` (tomorrow)
+
---
+++ countdown.js
@@ -0,0 +1,130 @@
+(function() {
+  'use strict';
+  
+  window.Countdown = window.Countdown || {};
+  
+  // Vanilla JS alternative to $.extend
+  window.extend = function(obj, extObj) {
+    if (arguments.length > 2) {
+        for (var a = 1; a < arguments.length; a++) {
+            extend(obj, arguments[a]);
+        }
+    } else {
+        for (var i in extObj) {
+            obj[i] = extObj[i];
+        }
+    }
+    return obj;
+  };
+  
+  // Countdown constructor
+  Countdown = function(conf) {
+    this.conf = extend({
+        // Dates
+        dateStart  : new Date(),
+        dateEnd    : new Date(new Date().getTime() + (24 * 60 * 60 * 1000)),
+         
+        // Default elements
+        selector   : ".timer",
+         
+        // Messages
+        msgBefore  : "Be ready!",
+        msgAfter   : "It's over, sorry folks!",
+        msgPattern : "{days} days, {hours} hours, {minutes} minutes and {seconds} seconds left."
+    }, conf || {});
+    
+    // Private variables
+    this.selector = document.querySelectorAll(this.conf.selector);
+    this.interval = 1000;
+    this.sec      = 0;
+    this.now      = new Date(); 
+    this.patterns = [
+      { pattern: '{years}', secs: 31536000 }, 
+      { pattern: '{months}', secs: 2628000 }, 
+      { pattern: '{weeks}', secs: 604800 }, 
+      { pattern: '{days}', secs: 86400 }, 
+      { pattern: '{hours}', secs: 3600 }, 
+      { pattern: '{minutes}', secs: 60 }, 
+      { pattern: '{seconds}', secs: 1 }
+    ];
+    
+    // Doing all the things!
+    this.init(); 
+  };
+  
+  // Initializing the instance
+  Countdown.prototype.init = function() {
+    this.defineInterval();
+      
+    this.now < this.conf.dateEnd && this.now >= this.conf.dateStart 
+    ? this.run() 
+    : this.outOfInterval();
+  };
+  
+  // Running the countdown
+  Countdown.prototype.run = function() {
+    var nowTS = this.now.valueOf() / 1000;
+    var tarTS = this.conf.dateEnd.valueOf() / 1000;
+    this.sec = Math.abs(tarTS - nowTS);
+    
+    // Vanilla JS alternative to $.proxy
+    var that = this;
+    var timer = window.setInterval(function() {
+      that.sec--;
+
+      if(that.sec > 0) {
+        that.display(that.sec);
+      } else {
+        that.outOfInterval();
+        clearInterval(timer);
+      }
+    }, this.interval);
+
+    this.display(this.sec);
+  };
+  
+  // Displaying the countdown
+  Countdown.prototype.display = function(sec) {
+    var output = this.conf.msgPattern;
+    
+      for (var i = 0, len = this.patterns.length; i < len; i++) {
+        var currentPattern = this.patterns[i];
+        
+        if (this.conf.msgPattern.indexOf(currentPattern.pattern) !== -1) {
+          var number = Math.floor(sec / currentPattern.secs);
+          sec -= number * currentPattern.secs;
+          output = output.replace(currentPattern.pattern, number);
+        }  
+      }
+          
+      for(var i = 0, len = this.selector.length; i < len; i++)
+         this.selector[i].innerHTML = output;
+  };
+ 
+  // Canceling the countdown in case it's over
+  Countdown.prototype.outOfInterval = function() {
+      var message = this.now > this.conf.dateStart ? this.conf.msgBefore : this.conf.msgAfter;
+      for(var i = 0, len = this.selector.length; i < len; i++)
+         this.selector[i].innerHTML = message;
+  };
+  
+  // Defining the interval to be used for refresh
+  Countdown.prototype.defineInterval = function() {
+    for (var i = this.patterns.length; i > 0; i--) { 
+      var currentPattern = this.patterns[i-1];
+        
+      if (this.conf.msgPattern.indexOf(currentPattern.pattern) !== -1) {
+        this.interval = currentPattern.secs * 1000;
+        return;
+      }
+    }
+  };
+}());
+
+// Initializing two countdowns
+var tomorrow     = new Countdown(); // All defaults
+var happyNewYear = new Countdown({  // Curstom
+  selector: '.new-year',
+  dateEnd: new Date('Jan 1, 2014 12:00:00'),
+  msgPattern : 'Happy new year in {days} days, {hours} hours, {minutes} minutes !'
+});
