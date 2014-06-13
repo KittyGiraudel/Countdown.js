8c6aefd5eef3d03643164f821719459ac4d445d3
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-06-13 18:16:17 +0200

    Code quality

--- countdown.js
+++ countdown.js
@@ -2,7 +2,7 @@
   "use strict";
 
   // Vanilla JS alternative to $.extend
-  global.extend = function(obj, extObj) {
+  global.extend = function (obj, extObj) {
     obj = obj || {};
     if (arguments.length > 2) {
       for (var a = 1; a < arguments.length; a++) {
@@ -17,7 +17,7 @@
   };
 
   // Countdown constructor
-  var Countdown = function(conf) {
+  var Countdown = function (conf) {
     this.conf = global.extend({
       // Dates
       dateStart    : new Date(),
@@ -58,31 +58,35 @@
   };
 
   // Initializing the instance
-  Countdown.prototype.init = function() {
+  Countdown.prototype.init = function () {
     this.defineInterval();
 
-    if(this.now < this.conf.dateEnd && this.now >= this.conf.dateStart) {
+    if (this.now < this.conf.dateEnd && this.now >= this.conf.dateStart) {
       this.run();
       this.callback("start");
-    } else {
+    } 
+
+    else {
       this.outOfInterval();
     }
   };
 
   // Running the countdown
-  Countdown.prototype.run = function() {
+  Countdown.prototype.run = function () {
     var now = this.now.valueOf() / 1000;
     var tar = this.conf.dateEnd.valueOf() / 1000;
     var sec = Math.abs(tar - now);
 
     // Vanilla JS alternative to $.proxy
     var that  = this;
-    var timer = global.setInterval(function() {
+    var timer = global.setInterval(function () {
       sec--;
 
-      if(sec > 0) {
+      if (sec > 0) {
         that.display(sec);
-      } else {
+      } 
+
+      else {
         global.clearInterval(timer);
         that.outOfInterval();
         that.callback("end");
@@ -93,7 +97,7 @@
   };
 
   // Displaying the countdown
-  Countdown.prototype.display = function(sec) {
+  Countdown.prototype.display = function (sec) {
     var output = this.conf.msgPattern;
 
     for (var b = 0; b < this.patterns.length; b++) {
@@ -107,13 +111,13 @@
       }
     }
 
-    for(var c = 0; c < this.selector.length; c++) {
+    for (var c = 0; c < this.selector.length; c++) {
       this.selector[c].innerHTML = output;
     }
   };
 
   // Defining the interval to be used for refresh
-  Countdown.prototype.defineInterval = function() {
+  Countdown.prototype.defineInterval = function () {
     for (var e = this.patterns.length; e > 0; e--) {
       var currentPattern = this.patterns[e-1];
 
@@ -125,31 +129,31 @@
   };
 
   // Canceling the countdown in case it's over
-  Countdown.prototype.outOfInterval = function() {
+  Countdown.prototype.outOfInterval = function () {
     var message = this.now < this.conf.dateStart ? this.conf.msgBefore : this.conf.msgAfter;
 
-    for(var d = 0; d < this.selector.length; d++) {
+    for (var d = 0; d < this.selector.length; d++) {
       this.selector[d].innerHTML = message;
     }
   };
 
   // Dealing with events and callbacks
-  Countdown.prototype.callback = function(event) {
+  Countdown.prototype.callback = function (event) {
     event = event.capitalize();
 
     // onStart callback
-    if(typeof this.conf["on" + event] === "function") {
+    if (typeof this.conf["on" + event] === "function") {
       this.conf["on" + event]();
     }
 
     // Triggering a jQuery event if jQuery is loaded
-    if(typeof global.jQuery !== "undefined") {
+    if (typeof global.jQuery !== "undefined") {
       global.jQuery(this.conf.selector).trigger("countdown" + event);
     }
   };
 
   // Adding a capitalize method to String
-  String.prototype.capitalize = function() {
+  String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
   };
 
